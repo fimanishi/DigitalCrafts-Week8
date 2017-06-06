cb875a1f260ce096de2bff8b668ad9be3273e15b
fimanishi <fimanishi@gmail.com>
2017-06-06 09:43:25 -0500

    express

---
+++ express.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express();
+
+app.get("/", function(request, response){
+  response.send("Hello World");
+});
+
+app.get("/about", function(request, response){
+  response.send("About me");
+});
+
+app.get("/projects", function(request, response){
+  response.send("My projects");
+});
+
+
+// URL Parameters
+app.get("/post/:slug", function(request,response){
+  var slug = request.params.slug;
+  response.send("Post about: " + slug);
+});
+
+// GET Query Parameters
+
+app.get("/hello", function(request, response){
+  var name = request.query.name || "World";
+  response.send("Hello " + name);
+});
+
+app.listen(8000, function(){
+  console.log("Listening on port 8000");
+});
